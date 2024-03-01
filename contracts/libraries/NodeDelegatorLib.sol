f792e28e4d3f94e102e7cf3326068e80f6c530e4
Nicholas Addison <nick@addisonbrown.com.au>
2024-03-01 17:35:39 +1100

    Fix linter

--- contracts/libraries/NodeDelegatorLib.sol
+++ contracts/libraries/NodeDelegatorLib.sol
@@ -15,8 +15,9 @@ import { ProxyFactory } from "script/foundry-scripts/utils/ProxyFactory.sol";
 library NodeDelegatorLib {
     function deployImpl() internal returns (address implementation) {
         address wethAddress = block.chainid == 1 ? Addresses.WETH_TOKEN : AddressesGoerli.WETH_TOKEN;
-        address eigenDelayWithdrawalRouterAddress =
-            block.chainid == 1 ? Addresses.EIGEN_DELAYED_WITHDRAWAL_ROUTER : AddressesGoerli.EIGEN_DELAYED_WITHDRAWAL_ROUTER;
+        address eigenDelayWithdrawalRouterAddress = block.chainid == 1
+            ? Addresses.EIGEN_DELAYED_WITHDRAWAL_ROUTER
+            : AddressesGoerli.EIGEN_DELAYED_WITHDRAWAL_ROUTER;
 
         // Deploy the new contract
         implementation = address(new NodeDelegator(wethAddress, eigenDelayWithdrawalRouterAddress));
@@ -45,8 +46,9 @@ library NodeDelegatorLib {
 
     function deployInit(uint256 index) internal returns (NodeDelegator nodeDelegator) {
         address wethAddress = block.chainid == 1 ? Addresses.WETH_TOKEN : AddressesGoerli.WETH_TOKEN;
-        address eigenDelayWithdrawalRouterAddress =
-            block.chainid == 1 ? Addresses.EIGEN_DELAYED_WITHDRAWAL_ROUTER : AddressesGoerli.EIGEN_DELAYED_WITHDRAWAL_ROUTER;
+        address eigenDelayWithdrawalRouterAddress = block.chainid == 1
+            ? Addresses.EIGEN_DELAYED_WITHDRAWAL_ROUTER
+            : AddressesGoerli.EIGEN_DELAYED_WITHDRAWAL_ROUTER;
 
         // Deploy the new contract
         address nodeDelegatorImpl = address(new NodeDelegator(wethAddress, eigenDelayWithdrawalRouterAddress));
