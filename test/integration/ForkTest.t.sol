5bad88b9a4daf262afe1156edbc955e64f78df89
Nicholas Addison <nick@addisonbrown.com.au>
2024-04-02 20:17:38 +1100

    Fixed fork tests by updating whale accounts for stETH and WETH

--- test/integration/ForkTest.t.sol
+++ test/integration/ForkTest.t.sol
@@ -32,7 +32,7 @@ contract ForkTestBase is Test {
     LRTConfig public lrtConfig;
     NodeDelegator public nodeDelegator1;
 
-    address internal constant stWhale = 0xd8d041705735cd770408AD31F883448851F2C39d;
+    address internal constant stWhale = 0xE53FFF67f9f384d20Ebea36F43b93DC49Ed22753;
     address internal constant xWhale = 0x1a0EBB8B15c61879a8e8DA7817Bb94374A7c4007;
     address internal constant oWhale = 0xEADB3840596cabF312F2bC88A4Bb0b93A4E1FF5F;
     address internal constant mWhale = 0xf89d7b9c864f589bbF53a82105107622B35EaA40;
@@ -143,7 +143,7 @@ contract ForkTestNative is ForkTestBase {
 
     address internal constant wWhale = 0xF04a5cC80B1E94C69B48f5ee68a08CD2F09A7c3E;
     // WETH whale that is not a contract
-    address internal constant wWhale2 = 0x267ed5f71EE47D3E45Bb1569Aa37889a2d10f91e;
+    address internal constant wWhale2 = 0x8EB8a3b98659Cce290402893d0123abb75E3ab28;
 
     // Get the result.validatorRegistrationTxs[0].data from the P2P Check Status Request
     // removed the 0x06e8fb9c function signature (4 bytes) from the validatorRegistrationTxs.data
