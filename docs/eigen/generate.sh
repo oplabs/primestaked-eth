87b89c3687915c687659da7d67ecb456f50dcab6
Nicholas Addison <nick@addisonbrown.com.au>
2024-04-04 12:54:49 +1100

    Generated diagrams of M2 EigenLayer contracts on Holesky

--- docs/eigen/generate.sh
+++ docs/eigen/generate.sh
@@ -1,62 +1,65 @@
-# EigneLayer Mainnet M1 contracts
+# EigneLayer M2 contracts on Holesky
 
 ## Strategy Manager
-sol2uml 0x5d25EEf8CfEdaA47d31fE2346726dE1c21e342Fb -v -o StrategyManager.svg
-sol2uml 0x5d25EEf8CfEdaA47d31fE2346726dE1c21e342Fb -v -hv -hf -he -hs -hl -hi -o  StrategyManagerHierarchy.svg
-sol2uml 0x5d25EEf8CfEdaA47d31fE2346726dE1c21e342Fb -v -s -d 0 -o  StrategyManagerSquashed.svg
-sol2uml 0x5d25EEf8CfEdaA47d31fE2346726dE1c21e342Fb -v -s -hp -hm -ht -d 0 -o  StrategyManagerPublicSquashed.svg
-sol2uml storage 0x5d25EEf8CfEdaA47d31fE2346726dE1c21e342Fb -v -c  StrategyManager -o StrategyManagerStorage.svg --hideExpand  __gap
-sol2uml storage 0x5d25EEf8CfEdaA47d31fE2346726dE1c21e342Fb -v -hv -d -s 0x858646372CC42E1A627fcE94aa7A7033e7CF075A \
+sol2uml 0x59f766A603C53f3AC8Be43bBe158c1519b193a18 -v -o StrategyManager.svg -n holesky
+sol2uml 0x59f766A603C53f3AC8Be43bBe158c1519b193a18 -v -hv -hf -he -hs -hl -hi -o  StrategyManagerHierarchy.svg -n holesky
+sol2uml 0x59f766A603C53f3AC8Be43bBe158c1519b193a18 -v -s -d 0 -o  StrategyManagerSquashed.svg -n holesky
+sol2uml 0x59f766A603C53f3AC8Be43bBe158c1519b193a18 -v -s -hp -hm -ht -d 0 -o  StrategyManagerPublicSquashed.svg -n holesky
+sol2uml storage 0x59f766A603C53f3AC8Be43bBe158c1519b193a18 -v -c  StrategyManager -o StrategyManagerStorage.svg --hideExpand  __gap -n holesky
+sol2uml storage 0x59f766A603C53f3AC8Be43bBe158c1519b193a18 -v -hv -d -s 0xdfB5f6CE42aAA7830E94ECFCcAd411beF4d4D5b6 -n holesky \
     -c StrategyManager -o StrategyManagerStorageData.svg --hideExpand  __gap \
     --slotNames 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc,0x4910fdfa16fed3260ed0e7147f7cc6da11a60208b5b9406d12a635614ffd9143,0xa3f0ad74e5423aebfd80d3ef4346578335a9a72aeaee59ff6cb3582b35133d50,0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103
-### diff M1 to M2 StrategyManager interface
-sol2uml diff 0x5d25EEf8CfEdaA47d31fE2346726dE1c21e342Fb 0x506C21f43e81D9d231d8A13831b42A2a2B5540E4 --bNetwork goerli -af "src/contracts/interfaces/IStrategyManager.sol"
+### M1 to M2 diff
+sol2uml diff 0x5d25EEf8CfEdaA47d31fE2346726dE1c21e342Fb 0x59f766A603C53f3AC8Be43bBe158c1519b193a18 --bNetwork holesky -af "src/contracts/interfaces/IStrategyManager.sol"
 
 ## Delegation Manager
-sol2uml 0xf97e97649da958d290e84e6d571c32f4b7f475e4 -v -o DelegationManager.svg
-sol2uml 0xf97e97649da958d290e84e6d571c32f4b7f475e4 -v -hv -hf -he -hs -hl -hi -o  DelegationManagerHierarchy.svg
-sol2uml 0xf97e97649da958d290e84e6d571c32f4b7f475e4 -v -s -d 0 -o  DelegationManagerSquashed.svg
-sol2uml 0xf97e97649da958d290e84e6d571c32f4b7f475e4 -v -s -hp -hm -ht -d 0 -o  DelegationManagerPublicSquashed.svg
-sol2uml storage 0xf97e97649da958d290e84e6d571c32f4b7f475e4 -v -c  DelegationManager -o DelegationManagerStorage.svg --hideExpand  __gap
-sol2uml storage 0xf97e97649da958d290e84e6d571c32f4b7f475e4 -v -d -s 0x39053d51b77dc0d36036fc1fcc8cb819df8ef37a \
+sol2uml 0x83f8F8f0BB125F7870F6bfCf76853f874C330D76 -v -o DelegationManager.svg -n holesky
+sol2uml 0x83f8F8f0BB125F7870F6bfCf76853f874C330D76 -v -hv -hf -he -hs -hl -hi -o  DelegationManagerHierarchy.svg -n holesky
+sol2uml 0x83f8F8f0BB125F7870F6bfCf76853f874C330D76 -v -s -d 0 -o  DelegationManagerSquashed.svg -n holesky
+sol2uml 0x83f8F8f0BB125F7870F6bfCf76853f874C330D76 -v -s -hp -hm -ht -d 0 -o  DelegationManagerPublicSquashed.svg -n holesky
+sol2uml storage 0x83f8F8f0BB125F7870F6bfCf76853f874C330D76 -v -c  DelegationManager -o DelegationManagerStorage.svg --hideExpand  __gap -n holesky
+sol2uml storage 0x83f8F8f0BB125F7870F6bfCf76853f874C330D76 -v -d -s 0xA44151489861Fe9e3055d95adC98FbD462B948e7 -n holesky \
     -c DelegationManager -o DelegationManagerStorageData.svg --hideExpand  __gap \
     --slotNames 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc,0x4910fdfa16fed3260ed0e7147f7cc6da11a60208b5b9406d12a635614ffd9143,0xa3f0ad74e5423aebfd80d3ef4346578335a9a72aeaee59ff6cb3582b35133d50,0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103
+### M1 to M2 diff
+sol2uml diff 0xf97E97649Da958d290e84E6D571c32F4b7F475e4 0x83f8F8f0BB125F7870F6bfCf76853f874C330D76 --bNetwork holesky -af "src/contracts/interfaces/IDelegationManager.sol"
+
 
 ## EigenPodManager
-sol2uml 0xEB86a5c40FdE917E6feC440aBbCDc80E3862e111 -v -o EigenPodManager.svg
-sol2uml 0xEB86a5c40FdE917E6feC440aBbCDc80E3862e111 -v -hv -hf -he -hs -hl -hi -o  EigenPodManagerHierarchy.svg
-sol2uml 0xEB86a5c40FdE917E6feC440aBbCDc80E3862e111 -v -s -d 0 -o  EigenPodManagerSquashed.svg
-sol2uml 0xEB86a5c40FdE917E6feC440aBbCDc80E3862e111 -v -s -hp -hm -ht -d 0 -o  EigenPodManagerPublicSquashed.svg
-sol2uml storage 0xEB86a5c40FdE917E6feC440aBbCDc80E3862e111 -v -c  EigenPodManager -o EigenPodManagerStorage.svg --hideExpand  __gap
-sol2uml storage 0xEB86a5c40FdE917E6feC440aBbCDc80E3862e111 -v -d -s 0x91E677b07F7AF907ec9a428aafA9fc14a0d3A338 \
+sol2uml 0x5265C162f7d5F3fE3175a78828ab16bf5E324a7B -v -o EigenPodManager.svg -n holesky
+sol2uml 0x5265C162f7d5F3fE3175a78828ab16bf5E324a7B -v -hv -hf -he -hs -hl -hi -o  EigenPodManagerHierarchy.svg -n holesky
+sol2uml 0x5265C162f7d5F3fE3175a78828ab16bf5E324a7B -v -s -d 0 -o  EigenPodManagerSquashed.svg -n holesky
+sol2uml 0x5265C162f7d5F3fE3175a78828ab16bf5E324a7B -v -s -hp -hm -ht -d 0 -o  EigenPodManagerPublicSquashed.svg -n holesky
+sol2uml storage 0x5265C162f7d5F3fE3175a78828ab16bf5E324a7B -v -c  EigenPodManager -o EigenPodManagerStorage.svg --hideExpand  __gap -n holesky
+sol2uml storage 0x5265C162f7d5F3fE3175a78828ab16bf5E324a7B -v -d -s 0x30770d7E3e71112d7A6b7259542D1f680a70e315 -n holesky \
     -c EigenPodManager -o EigenPodManagerStorageData.svg --hideExpand  __gap \
     --slotNames 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc,0x4910fdfa16fed3260ed0e7147f7cc6da11a60208b5b9406d12a635614ffd9143,0xa3f0ad74e5423aebfd80d3ef4346578335a9a72aeaee59ff6cb3582b35133d50,0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103
-### diff M1 to M2 EigenPodManager interface
-sol2uml diff 0xEB86a5c40FdE917E6feC440aBbCDc80E3862e111 0xDA9B60D3dC7adD40C0e35c628561Ff71C13a189f --bNetwork goerli -af "src/contracts/interfaces/IEigenPodManager.sol"
+### M1 to M2 diff
+sol2uml diff 0xEB86a5c40FdE917E6feC440aBbCDc80E3862e111 0x5265C162f7d5F3fE3175a78828ab16bf5E324a7B --bNetwork holesky -af "src/contracts/interfaces/IEigenPodManager.sol"
 
 
 ## StrategyBaseTVLLimits
-sol2uml 0xdfdA04f980bE6A64E3607c95Ca26012Ab9aA46d3 -v -o Strategy.svg
-sol2uml 0xdfdA04f980bE6A64E3607c95Ca26012Ab9aA46d3 -v -hv -hf -he -hs -hl -hi -o  StrategyHierarchy.svg
-sol2uml 0xdfdA04f980bE6A64E3607c95Ca26012Ab9aA46d3 -v -s -hp -hm -ht -d 0 -o  StrategyPublicSquashed.svg
-sol2uml 0xdfdA04f980bE6A64E3607c95Ca26012Ab9aA46d3 -v -s -d 0 -o  StrategySquashed.svg
-sol2uml storage 0xdfdA04f980bE6A64E3607c95Ca26012Ab9aA46d3 -v -c  StrategyBaseTVLLimits -o StrategyStorage.svg --hideExpand  __gap
-# stETH Strategy
-sol2uml storage 0xdfdA04f980bE6A64E3607c95Ca26012Ab9aA46d3 -v -d -s 0x93c4b944D05dfe6df7645A86cd2206016c51564D \
+sol2uml 0xFb83e1D133D0157775eC4F19Ff81478Df1103305 -v -o Strategy.svg -n holesky
+sol2uml 0xFb83e1D133D0157775eC4F19Ff81478Df1103305 -v -hv -hf -he -hs -hl -hi -o  StrategyHierarchy.svg -n holesky
+sol2uml 0xFb83e1D133D0157775eC4F19Ff81478Df1103305 -v -s -hp -hm -ht -d 0 -o  StrategyPublicSquashed.svg -n holesky
+sol2uml 0xFb83e1D133D0157775eC4F19Ff81478Df1103305 -v -s -d 0 -o  StrategySquashed.svg -n holesky
+sol2uml storage 0xFb83e1D133D0157775eC4F19Ff81478Df1103305 -v -c  StrategyBaseTVLLimits -o StrategyStorage.svg --hideExpand  __gap -n holesky
+# M2 on holesky stETH Strategy
+sol2uml storage 0xFb83e1D133D0157775eC4F19Ff81478Df1103305 -v -d -s 0x7D704507b76571a51d9caE8AdDAbBFd0ba0e63d3 -n holesky \
     -c StrategyBaseTVLLimits -o stETHStrategyStorageData.svg --hideExpand  __gap \
     --slotNames 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc,0x4910fdfa16fed3260ed0e7147f7cc6da11a60208b5b9406d12a635614ffd9143,0xa3f0ad74e5423aebfd80d3ef4346578335a9a72aeaee59ff6cb3582b35133d50,0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103
-## OETH Strategy
+## M1 on mainnet OETH Strategy
 sol2uml storage 0xdfdA04f980bE6A64E3607c95Ca26012Ab9aA46d3 -v -d -s 0xa4C637e0F704745D182e4D38cAb7E7485321d059 \
     -c StrategyBaseTVLLimits -o OETHStrategyStorageData.svg --hideExpand  __gap \
     --slotNames 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc,0x4910fdfa16fed3260ed0e7147f7cc6da11a60208b5b9406d12a635614ffd9143,0xa3f0ad74e5423aebfd80d3ef4346578335a9a72aeaee59ff6cb3582b35133d50,0xb53127684a568b3173ae13b9f8a6016e243e63b6e8ee1178d6a717850b5d6103
-### diff M1 to M2 StrategyBaseTVLLimits interface
-sol2uml diff 0xdfdA04f980bE6A64E3607c95Ca26012Ab9aA46d3 0x81E94e16949AC397d508B5C2557a272faD2F8ebA --bNetwork goerli -af "src/contracts/interfaces/IStrategyManager.sol"
+### M1 to M2 diff
+sol2uml diff 0xdfdA04f980bE6A64E3607c95Ca26012Ab9aA46d3 0xFb83e1D133D0157775eC4F19Ff81478Df1103305 --bNetwork holesky -af "src/contracts/interfaces/IStrategyManager.sol"
 
 # Slasher
-sol2uml 0xef31c292801f24f16479DD83197F1E6AeBb8d6d8 -v -o Slasher.svg
-sol2uml 0xef31c292801f24f16479DD83197F1E6AeBb8d6d8 -v -hv -hf -he -hs -hl -hi -o  SlasherHierarchy.svg
-sol2uml 0xef31c292801f24f16479DD83197F1E6AeBb8d6d8 -v -s -d 0 -o  SlasherSquashed.svg
-sol2uml 0xef31c292801f24f16479DD83197F1E6AeBb8d6d8 -v -s -hp -hm -ht -d 0 -o  SlasherPublicSquashed.svg
-sol2uml storage 0xef31c292801f24f16479DD83197F1E6AeBb8d6d8 -v -c  Slasher -o SlasherStorage.svg --hideExpand  __gap
-sol2uml storage 0xef31c292801f24f16479DD83197F1E6AeBb8d6d8 -v -d -s 0xD92145c07f8Ed1D392c1B88017934E301CC1c3Cd -c Slasher -o SlasherStorageData.svg --hideExpand  __gap
+sol2uml 0x99715D255E34a39bE9943b82F281CA734bcF345A -v -o Slasher.svg -n holesky
+sol2uml 0x99715D255E34a39bE9943b82F281CA734bcF345A -v -hv -hf -he -hs -hl -hi -o  SlasherHierarchy.svg -n holesky
+sol2uml 0x99715D255E34a39bE9943b82F281CA734bcF345A -v -s -d 0 -o  SlasherSquashed.svg -n holesky
+sol2uml 0x99715D255E34a39bE9943b82F281CA734bcF345A -v -s -hp -hm -ht -d 0 -o  SlasherPublicSquashed.svg -n holesky
+sol2uml storage 0x99715D255E34a39bE9943b82F281CA734bcF345A -v -c  Slasher -o SlasherStorage.svg --hideExpand  __gap -n holesky
+sol2uml storage 0x99715D255E34a39bE9943b82F281CA734bcF345A -v -d -s 0xcAe751b75833ef09627549868A04E32679386e7C -c Slasher -o SlasherStorageData.svg --hideExpand  __gap -n holesky
 
