a5204e5035e982db89c45b3c489287b09773d9d2
Nicholas Addison <nick@addisonbrown.com.au>
2024-04-18 12:55:55 +1000

    Added basic mainnet fork tests for LST withdrawals

--- test/integration/ForkHoleskyTest.sol
+++ test/integration/ForkHoleskyTest.sol
@@ -931,6 +931,12 @@ contract ForkHoleskyTestLSTWithdrawals is ForkHoleskyTestBase {
             "decoded WithdrawalQueued"
         );
         assertEq(requestLogs[1].topics[0], 0x9009ab153e8014fbfb02f2217f5cde7aa7f9ad734ae85ca3ee3f4ca2fdd499f9);
+        // Decode the withdrawal data
+        (bytes32 withdrawalRoot, IDelegationManager.Withdrawal memory withdrawal) =
+            abi.decode(requestLogs[1].data, (bytes32, IDelegationManager.Withdrawal));
+        assertEq(withdrawal.staker, address(nodeDelegator1));
+        assertEq(withdrawal.delegatedTo, address(0));
+        assertEq(withdrawal.withdrawer, address(nodeDelegator1));
 
         // WithdrawalRequested event on the LRTDepositPool contract
         assertEq(
@@ -1199,7 +1205,7 @@ contract ForkHoleskyTestLSTWithdrawals is ForkHoleskyTestBase {
 
         // Move forward 10 blocks
         // DelegationManager.minWithdrawalDelayBlocks on Holesky is 10
-        vm.roll(block.number + 11);
+        vm.roll(block.number + 10);
 
         (uint256 assetsInDepositPoolBefore, uint256 assetsInNDCsBefore, uint256 assetsInEigenLayerBefore) =
             lrtDepositPool.getAssetDistributionData(stETHAddress);
@@ -1262,7 +1268,7 @@ contract ForkHoleskyTestLSTWithdrawalsClaim is ForkHoleskyTestBase {
 
         // Move forward 10 blocks
         // DelegationManager.minWithdrawalDelayBlocks on Holesky is 10
-        vm.roll(block.number + 11);
+        vm.roll(block.number + 10);
     }
 
     ////////////////////////////////////////////////////////////////
@@ -1419,7 +1425,7 @@ contract ForkHoleskyTestInternalLSTWithdrawalsClaim is ForkHoleskyTestBase {
 
         // Move forward 10 blocks
         // DelegationManager.minWithdrawalDelayBlocks on Holesky is 10
-        vm.roll(block.number + 11);
+        vm.roll(block.number + 10);
     }
 
     function test_claimInternalWithdrawalFull() external {
