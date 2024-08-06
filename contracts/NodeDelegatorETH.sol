21ac1f7ca9014e403aae83b0adfa67baf4651bab
Nicholas Addison <nick@addisonbrown.com.au>
2024-08-06 11:42:51 +1000

    Removed unused IAccessControl import

--- contracts/NodeDelegatorETH.sol
+++ contracts/NodeDelegatorETH.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.21;
 
-import { IAccessControl } from "@openzeppelin/contracts/access/IAccessControl.sol";
 import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
 import { PausableUpgradeable } from "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
 import { ReentrancyGuardUpgradeable } from "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
